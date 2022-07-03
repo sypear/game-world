70ef5f13bbbbeedc2d35b1d30d94452b59e8e26a
seungyeon.bae <bsyeon015@gmail.com>
2022-07-03 22:24:51 +0900

    :sparkles: Life(남은 대전 기회), 게임 진행 횟수에 따른 스피드 조절 기능 추가

--- js/game-rps.js
+++ js/game-rps.js
@@ -1,5 +1,18 @@
 "use strict";
 
+// 게임 기본 스피드 0.3초로 설정
+let speed = 300;
+
+// 게임 회차 및 승패 정보 저장
+let count = 0;
+let winCount = 0;
+let drawCount = 0;
+let loseCount = 0;
+
+// 게임 시작 시 기회 3번으로 시작
+const playerLifeItem = document.getElementById("player-life");
+let playerLife = 3;
+
 // PC의 가위, 바위, 보 선택을 위한 함수 (가위 : 0, 바위 : 1, 보 : 2)
 const pcImage = document.getElementById("pc-image");
 
@@ -72,6 +85,9 @@ buttonWrapper.addEventListener("click", function(e) {
 
 // 가위 바위 보 메인 계산 함수
 function rockPaperSissors(playerSelection) {
+    // 게임 카운트 +1
+    count++;
+
     // Interval 정지
     clearInterval(timer);
 
@@ -81,7 +97,12 @@ function rockPaperSissors(playerSelection) {
     // 대진 결과 화면에 출력
     showMatchResult(result, playerSelection, pcSelection);
 
-    // 화면에 모달 창 출력
+    // 기회가 없으면 게임 종료
+    if (playerLife === 0) {
+        resetSettings();
+    }
+
+    // 결과 모달 출력
     modal.classList.add("show");
 
     // 모달 종료 시 게임 재시작
@@ -93,10 +114,13 @@ function checkMatchResult(player, pc) {
     let result = player - pc;
 
     if (result === 0) {                              // 무승부인 경우
+        drawCount++;
         return 1;
     } else if (result === -2 || result === 1) {       // 사용자가 승리한 경우
+        winCount++;
         return 2;
     } else if (result === -1 || result === 2) {       // 사용자가 패배한 경우
+        loseCount++;
         return 0;
     }
 }
@@ -108,7 +132,6 @@ const modalTitle = document.getElementsByClassName("modal__content-title")[0];
 const bestScoreItem = document.getElementById("best-score");
 const playerScoreItem = document.getElementById("score-player");
 const pcScoreItem = document.getElementById("score-pc");
-const test = document.getElementsByClassName("modal__content-title--result")[0];
 
 let bestScore = 0;
 let playerScore = 0;
@@ -120,20 +143,52 @@ function showMatchResult(result, player, pc) {
         calculateScore(result);
     }
 
-    // 대진 결과 대입
-    let colorList = ["color-red", "color-green", "color-blue"];
-    let resultList = ["패배", "무승부", "승리"];
-    let rpsList = ["✌", "✊", "✋"];
+    // 남은 기회 갱신 (이기면 +1, 지면 -1)
+    if (result === 0) {
+        playerLife -= 1;
+    } else if (result === 2) {
+        playerLife += 1;
+    }
+    playerLifeItem.innerText = playerLife;
 
-    modalTitle.innerHTML = `
-        <h1 class="modal__content-title--result ${colorList[result]}">
-            ${resultList[result]}
-        </h1>
-        <p class="modal__content-title--desc">
-            PC : ${rpsList[pc]}<br />
-            Player : ${rpsList[player]}
-        </p>
-    `;
+    // 모달에 대진 결과 대입
+    if (playerLife > 0) {
+        let colorList = ["color-red", "color-green", "color-blue"];
+        let lifeList = ["❤️-1", "", "❤️+1"]
+        let resultList = ["패배", "무승부", "승리"];
+        let rpsList = ["✌", "✊", "✋"];
+
+        modalTitle.innerHTML = `
+            <h1 class="modal__content-title--result ${colorList[result]}">
+                ${resultList[result]}!<br />
+            </h1>
+            <span class="modal__content-title--result-life">${lifeList[result]}</span>
+            <p class="modal__content-title--desc">
+                PC : ${rpsList[pc]}<br />
+                Player : ${rpsList[player]}
+            </p>
+        `;
+
+        const resultLifeItem = document.getElementsByClassName("modal__content-title--result-life")[0];
+
+        resultLifeItem.style.animation = "blinkingEffect 400ms 6 alternate";
+    } else {
+        modalTitle.innerHTML = `
+            <h1 class="modal__content-title--result color-red">
+                게임 종료!
+            </h1>
+            <span class="modal__content-title--score">
+                기록 : <strong>${playerScore}</strong>점
+            </span>
+            <p class="modal__content-title--desc">
+                총 ${count}번의 대결 동안<br />
+                <span class="color-blue">${winCount}번</span>의 승리<br />
+                <span class="color-red">${loseCount}번</span>의 패배<br />
+                <span class="color-green">${drawCount}번</span>의 무승부가<br />
+                있었습니다.
+            </p>
+        `;
+    }
 }
 
 // 점수 계산 후 화면에 갱신하는 함수
@@ -187,14 +242,37 @@ function restartGame() {
     // 컴퓨터의 마지막 선택 값 재설정
     changePcSelection();
 
+    // 화면 초기화
+    playerLifeItem.innerText = playerLife;
+    playerScoreItem.innerText = playerScore;
+    pcScoreItem.innerText = pcScore;
+
+    // 게임 회차에 따라 스피드 빠르게 조절 (20회차 이상 부턴 속도 고정)
+    if (count <= 20) {
+        speed = speed - 10;
+    }
+
     // 게임 재시작
-    timer = setInterval(changePcSelection, 300);
+    timer = setInterval(changePcSelection, speed);
+}
+
+// 게임에 필요한 설정 값 초기화 함수
+function resetSettings() {
+    speed = 300;
+    playerLife = 3;
+    playerScore = 0;
+    pcScore = 0;
+    count = 0;
+    winCount = 0;
+    drawCount = 0;
+    loseCount = 0;
 }
 
 // 기본 값 세팅 및 가위바위보 게임 자동 시작
 window.onload = function() {
-    timer = setInterval(changePcSelection, 300);
+    timer = setInterval(changePcSelection, speed);
 
+    playerLifeItem.innerText = playerLife;
     // bestScoreItem.innerText = bestScore;
     playerScoreItem.innerText = playerScore;
     pcScoreItem.innerText = pcScore;
