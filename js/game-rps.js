88319f7d59d3532299623ad45549f6835d7cffd0
seungyeon.bae <bsyeon015@gmail.com>
2022-07-03 15:15:17 +0900

    :sparkles: 대진 결과 출력 및 점수 갱신 기능 추가

--- js/game-rps.js
+++ js/game-rps.js
@@ -4,9 +4,9 @@
 const pcImage = document.getElementById("pc-image");
 
 let timer = 0;
-let selection = ['scissors', 'rock', 'paper'];
-let lastPcSelection = '';
-let pcSelection = '';
+let selection = ["scissors", "rock", "paper"];
+let lastPcSelection = "";
+let pcSelection = "";
 
 function changePcSelection() {
     // 생성된 난수를 PC의 선택 값으로 설정
@@ -35,8 +35,10 @@ const scissorsButton = document.getElementById("scissors-button");
 const rockButton = document.getElementById("rock-button");
 const papersButton = document.getElementById("paper-button");
 
+let closeTimerState = 0;
+
 buttonWrapper.addEventListener("click", function(e) {
-    let playerSelection = '';
+    let playerSelection = "";
 
     // 사용자가 선택한 버튼에 따라 사용자 선택 값 설정
     if (e.target === scissorsButton) {
@@ -47,18 +49,26 @@ buttonWrapper.addEventListener("click", function(e) {
         playerSelection = 2;
     }
 
-    // setInterval 정지
+    rockPaperSissors(playerSelection);
+});
+
+// 가위 바위 보 메인 계산 함수
+function rockPaperSissors(playerSelection) {
+    // Interval 정지
     clearInterval(timer);
 
     // 대진 결과 판단 (사용자 패 : -1, 무 : 0, 사용자 승 : 1)
     let result = checkMatchResult(playerSelection, pcSelection);
-    if (result !== 0) {
-        calculateScore(result);
-    }
 
-    // 게임 재 시작
-    timer = setInterval(changePcSelection, 300);
-});
+    // 대진 결과 화면에 출력
+    showMatchResult(result);
+
+    // 화면에 모달 창 출력
+    modal.classList.add("show");
+
+    // 모달 종료 시 게임 재시작
+    restartGameAfterExitModal();
+}
 
 // 대진 결과를 판단하는 함수 (가위 : 0, 바위 : 1, 보 : 2)
 function checkMatchResult(player, pc) {
@@ -73,7 +83,10 @@ function checkMatchResult(player, pc) {
     }
 }
 
-// 점수 계산 함수
+// 대진 결과를 화면에 출력하는 함수
+const modal = document.getElementsByClassName("modal")[0];
+const modalTitle = document.getElementsByClassName("modal__content-title")[0];
+
 const bestScoreItem = document.getElementById("best-score");
 const playerScoreItem = document.getElementById("score-player");
 const pcScoreItem = document.getElementById("score-pc");
@@ -82,20 +95,87 @@ let bestScore = 0;
 let playerScore = 0;
 let pcScore = 0;
 
+function showMatchResult(result) {
+    // 화면에 점수 갱신
+    if (result !== 0) {
+        calculateScore(result);
+    }
+
+    // 대진 결과 대입
+    if (result === 1) {
+        modalTitle.innerHTML = `
+            <h1>WIN</h1>
+            축하합니다!
+        `;
+    } else if (result === 0) {
+        modalTitle.innerHTML = `
+            <h1>DRAW</h1>
+            아쉬워요!
+        `;
+    } else if (result === -1) {
+        modalTitle.innerHTML = `
+            <h1>LOSE</h1>
+            패배!
+        `;
+    }
+}
+
+// 점수 계산 후 화면에 갱신하는 함수
 function calculateScore(result) {
     if (result === 1) {
-        console.log('이김');
         playerScore += 10;
         playerScoreItem.innerText = playerScore;
     } else if (result === -1) {
-        console.log('짐');
         pcScore += 10;
         pcScoreItem.innerText = pcScore;
     }
 }
 
-// 기본 값 세팅 및 가위바위보 게임 자동 시작
+// 5초 동안 결과를 출력하는 모달 창이 닫히면 게임을 재시작 하는 함수
+const timeRemain = document.getElementById("time-remain");
 
+let closeTimer = 0;
+let time = 5;
+
+function restartGameAfterExitModal() {
+    timeRemain.innerText = time;
+
+    closeTimer = setInterval(() => {
+        timeRemain.innerText = --time;
+
+        if (time == 0) {
+            modal.classList.remove("show");
+            restartGame();
+        }
+    }, 1000);
+}
+
+// 5초 되기 전에 사용자가 수동으로 모달 창을 종료하는 경우
+const modalCloseButton = document.getElementsByClassName("modal__content-close-button")[0];
+
+modal.addEventListener('click', function(e) {
+    if (e.target === modal || e.target === modalCloseButton) {
+        modal.classList.remove("show");
+        restartGame();
+    }
+});
+
+// 게임을 재시작 하는 함수
+function restartGame() {
+    // 결과 모달 출력 타이머 종료
+    clearInterval(closeTimer);
+
+    // 결가 모달 time 초기화
+    time = 5;
+
+    // 컴퓨터의 마지막 선택 값 재설정
+    changePcSelection();
+
+    // 게임 재시작
+    timer = setInterval(changePcSelection, 300);
+}
+
+// 기본 값 세팅 및 가위바위보 게임 자동 시작
 window.onload = function() {
     timer = setInterval(changePcSelection, 300);
 
